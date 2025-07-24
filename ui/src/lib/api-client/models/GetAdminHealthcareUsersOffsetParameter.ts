07145a6fe5d2f39b66f826c97ff9e9785815e4df
quotentiroler <nussbaumer.max@hotmail.com>
2025-07-24 22:58:40 +0200

    update

--- ui/src/lib/api-client/models/GetAdminHealthcareUsersOffsetParameter.ts
+++ ui/src/lib/api-client/models/GetAdminHealthcareUsersOffsetParameter.ts
@@ -13,6 +13,14 @@
  */
 
 import { mapValues } from '../runtime';
+import type { GetAdminHealthcareUsersOffsetParameterAnyOf } from './GetAdminHealthcareUsersOffsetParameterAnyOf';
+import {
+    GetAdminHealthcareUsersOffsetParameterAnyOfFromJSON,
+    GetAdminHealthcareUsersOffsetParameterAnyOfFromJSONTyped,
+    GetAdminHealthcareUsersOffsetParameterAnyOfToJSON,
+    GetAdminHealthcareUsersOffsetParameterAnyOfToJSONTyped,
+} from './GetAdminHealthcareUsersOffsetParameterAnyOf';
+
 /**
  * 
  * @export
